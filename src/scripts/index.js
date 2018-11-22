3d2b4298e974e5d250ec96bfb03fb0d9f5fb278e
Stephen Buchanan <stephen.buchanan@random.studio>
2018-11-22 13:57:53 +0100

    lightweight button click handling

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,5 +5,18 @@ import * as Tone from 'tone';
 //create a synth and connect it to the master output (your speakers)
 const synth = new Tone.Synth().toMaster();
 
-//play a middle 'C' for the duration of an 8th note
-synth.triggerAttackRelease("C4", "1n");
+const playNote = () => {
+    synth.triggerAttackRelease("C4", "4n");
+};
+
+const createButton = (name, label, clickHandler, parent = document.body) => {
+    const button = document.createElement('button');
+    button.value = name;
+    button.label = name;
+    button.appendChild(document.createTextNode(label));
+    button.onclick = clickHandler;
+    parent.appendChild(button);
+    return button;
+};
+
+createButton('play', 'play note', playNote);
