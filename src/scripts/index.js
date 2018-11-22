a6f0c2553a9f8e1b31c49a4c24b2e1c038cdea89
Stephen Buchanan <stephen.buchanan@random.studio>
2018-11-22 13:36:35 +0100

    very basic webpack minimal tonjes example

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,9 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+import * as Tone from 'tone';
+
+//create a synth and connect it to the master output (your speakers)
+const synth = new Tone.Synth().toMaster();
+
+//play a middle 'C' for the duration of an 8th note
+synth.triggerAttackRelease("C4", "1n");
